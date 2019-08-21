52da113b67c04b2509e8a7f8181b27d4d0f09af2
Kumar <abhishekx.x.kumar@intel.com>
2019-08-21 16:40:40 +0530

    save the logic

--- constants.py
+++ constants.py
@@ -7,3 +7,7 @@ dt_time=datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 now_time=datetime.now()
 
 q_address_input="""select * from address_input where needs_lookup=1;"""
+
+updatequery_address_input = """ UPDATE address_input
+                SET needs_lookup = 0
+                WHERE address_id = %s ;"""
